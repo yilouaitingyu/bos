cfe3b8168f8aa1afb191e3d5d520765dba2609b6
huchao <1970205750@qq,com>
2017-08-02 10:09:00 +0800

    测试合并分支

---
+++ bosPost/src/main/java/cn/huchao/action/Pra1.java
@@ -0,0 +1,15 @@
+package cn.huchao.action;
+
+/**
+ * @author huchao
+ * @2017年8月2日
+ * @description
+ * 
+ */
+public class Pra1 {
+	public static void main(String[] args) {
+		//操作分支合并
+		String aa ="22";
+		System.out.println(aa);
+	}
+}
